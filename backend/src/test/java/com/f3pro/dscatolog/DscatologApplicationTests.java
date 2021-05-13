e3264cf7e5205a9627c62c31317c74c04a534905
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-05-13 20:25:58 -0300

    Project created

---
+++ backend/src/test/java/com/f3pro/dscatolog/DscatologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f3pro.dscatolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
