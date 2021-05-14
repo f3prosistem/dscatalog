9374de7d535184a0b201d4147984aa1ca1e981a3
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-05-14 20:48:38 -0300

    DTO

--- backend/src/main/java/com/f3pro/dscatolog/resources/CategoryResource.java
+++ backend/src/main/java/com/f3pro/dscatolog/resources/CategoryResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.f3pro.dscatolog.dto.CategoryDTO;
 import com.f3pro.dscatolog.entities.Category;
 import com.f3pro.dscatolog.services.CategoryService;
 
@@ -20,8 +21,8 @@ public class CategoryResource {
 	private CategoryService service; 
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/f3pro/dscatolog/services/CategoryService.java
+++ backend/src/main/java/com/f3pro/dscatolog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.f3pro.dscatolog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.f3pro.dscatolog.dto.CategoryDTO;
 import com.f3pro.dscatolog.entities.Category;
 import com.f3pro.dscatolog.repositories.CategoryRepository;
 
@@ -16,10 +18,11 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	// listar todas as categorias no banco de dado.
-	
+
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 
 	}
 
