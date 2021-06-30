dc462b0fe17c55a6048021b065148e10d2e54fa4
Francivaldo Souza <francivaldo.alves@gmail.com>
2021-06-30 20:59:30 -0300

    Custom validation response

--- backend/src/main/java/com/f3pro/dscatolog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/f3pro/dscatolog/resources/exceptions/ResourceExceptionHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -31,7 +33,7 @@ public class ResourceExceptionHandler {
 
 	@ExceptionHandler(DatabaseExeption.class)
 	public ResponseEntity<StanderdError> database(DatabaseExeption e, HttpServletRequest request) {
-		
+
 		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StanderdError err = new StanderdError();
 		err.setTimestamp(Instant.now());
@@ -39,6 +41,23 @@ public class ResourceExceptionHandler {
 		err.setError("Exceção de banco de dados");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError err = new ValidationError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Exceção na validação");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		for (FieldError f : e.getBindingResult().getFieldErrors()) {
+			err.addError(f.getField(), f.getDefaultMessage());
+		}
+
 		return ResponseEntity.status(status).body(err);
 	}
 
